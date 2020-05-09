b116d1aafa67dcff5ba5f8a9dfa9145cc7806090
Sheila Gaynor <sheilagaynor@hsph.harvard.edu>
2020-05-08 23:58:11 -0400

    update data url

--- preprocess_data/preprocess_covidtracking.R
+++ preprocess_data/preprocess_covidtracking.R
@@ -3,7 +3,7 @@ library (readr)
 
 #Inputs: level (County or State), pull date from Covid tracking ("md" - note not daily), start date for the data period ("%Y-%m-%d"), end date for the data period ("%Y-%m-%d")
 load_covid_tracking <- function( level, pull_date="0505", start_date, end_date ){
-  covid_tracking_url <- paste0("https://raw.githubusercontent.com/lin-lab/covid-data-cleaning/master/covid-tracking_data/covidtracking_cleaned/covidtracking_",level,"_",pull_date,".csv")
+  covid_tracking_url <- paste0("https://raw.githubusercontent.com/lin-lab/COVID-data-cleaning/master/covid-tracking_data/covidtracking_cleaned/covidtracking_",level,"_",pull_date,".csv")
   input_data <- read.csv(url(covid_tracking_url), stringsAsFactors = F)
   covid_tracking_out <- input_data[ as.Date(input_data$date) >= start_date & as.Date(input_data$date) <= end_date, ]
   return(covid_tracking_out)
--- preprocess_data/preprocess_jhu.R
+++ preprocess_data/preprocess_jhu.R
@@ -3,7 +3,7 @@ library (readr)
 
 #Inputs: level (County, State, or USA), pull date from jhu ("%Y-%m-%d"), start date for the data period ("%Y-%m-%d"), end date for the data period ("%Y-%m-%d")
 load_jhu <- function( level, pull_date=format(Sys.Date(), "%Y-%m-%d"), start_date, end_date ){
-  jhu_url <- paste0("https://raw.githubusercontent.com/lin-lab/COVID-data-cleaning/master/jhu_data/cleaned_data/jhu_COVID-19_",level,"_",pull_date,".csv")
+  jhu_url <- paste0("https://raw.githubusercontent.com/lin-lab/COVID-data-cleaning/master/jhu_data/cleaned_data/JHU_COVID-19_",level,"_",pull_date,".csv")
   input_data <- read.csv(url(jhu_url), stringsAsFactors = F)
   jhu_out <- input_data[ as.Date(input_data$date) >= start_date & as.Date(input_data$date) <= end_date, ]
   return(jhu_out)
