d02e8da10b231f422550127a3a7934a6fc9a1d8b
Sheila Gaynor <sheilagaynor@hsph.harvard.edu>
2020-05-11 12:40:01 -0400

    add temp initial estimate script

---
+++ initial_estimates/calc_initial_estimates.R
@@ -0,0 +1,29 @@
+source('~/COVID19-Rt/preprocess_data/preprocess_jhu.R')
+source('~/COVID19-Rt/estimate_rt/estimate_rt_master.R')
+
+jhu_counties <- load_jhu(level='County',pull_date = '2020-05-08', start_date = '2020-03-01', end_date = '2020-05-01')
+jhu_states <- load_jhu(level='State',pull_date = '2020-05-08', start_date = '2020-03-01', end_date = '2020-05-01')
+
+state_rt <- c()
+for (ii in 1:length(unique(jhu_states$stateName))){
+  jhu_state_sub <- jhu_states[jhu_states$stateName==unique(jhu_states$stateName)[ii],]
+  state_rt <- rbind(state_rt,
+  cbind(estimate_rt_EpiEstim(jhu_state_sub$date, jhu_state_sub$positiveIncrease, mean_serial = 4.5, std_serial = 3.5), unique(jhu_states$stateName)[ii]))
+}
+names(state_rt)[c(2,5)] <- c('date','stateName')
+state_rt$date <- as.character(state_rt$date)
+jhu_state_out <- merge(jhu_states, state_rt, by=c('stateName','date'))
+
+
+county_rt <- c()
+for (ii in 1:length(unique(jhu_counties$FIPS))){
+  jhu_county_sub <- jhu_counties[jhu_counties$FIPS==unique(jhu_counties$FIPS)[ii],]
+  county_rt <- rbind(county_rt,
+                    cbind(estimate_rt_EpiEstim(jhu_county_sub$date, jhu_county_sub$positiveIncrease, mean_serial = 4.5, std_serial = 3.5), unique(jhu_counties$FIPS)[ii]))
+}
+names(county_rt)[c(2,5)] <- c('date','FIPS')
+county_rt$date <- as.character(county_rt$date)
+jhu_county_out <- merge(jhu_counties, county_rt, by=c('FIPS','date'))
+
+write.table(jhu_county_out, "~/COVID19-Rt/initial_estimates/jhu_county_rt.csv", quote = F, row.names = F, col.names = T, sep=',')
+write.table(jhu_state_out, "~/COVID19-Rt/initial_estimates/jhu_state_rt.csv", quote = F, row.names = F, col.names = T, sep=',')
