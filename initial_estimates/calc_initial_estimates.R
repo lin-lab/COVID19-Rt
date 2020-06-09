bfac6083a58ff5b24c54fb1aa56d99697a31e2bf
Andy Shi <andyshi@g.harvard.edu>
2020-06-08 20:24:04 -0400

    Get rid of pull_date

--- initial_estimates/calc_initial_estimates.R
+++ initial_estimates/calc_initial_estimates.R
@@ -1,15 +1,16 @@
-source('./COVID19-Rt/preprocess_data/preprocess_jhu.R')
-source('./COVID19-Rt/estimate_rt/estimate_rt_master.R')
+source('../preprocess_data/preprocess_jhu.R')
+source('../estimate_rt/estimate_rt_master.R')
 
 #Load in data from data-cleaning github
-jhu_counties <- load_jhu(level='County',pull_date = '2020-06-01', start_date = '2020-03-01', end_date = '2020-05-29')
-jhu_states <- load_jhu(level='State',pull_date = '2020-06-01', start_date = '2020-03-01', end_date = '2020-05-29')
-jhu_global <- load_jhu(level='Global',pull_date = '2020-06-01', start_date = '2020-03-01', end_date = '2020-05-29')
+jhu_counties <- load_jhu(level='County', start_date = '2020-03-01', end_date = '2020-06-08')
+jhu_states <- load_jhu(level='State',  start_date = '2020-03-01', end_date = '2020-06-08')
+jhu_global <- load_jhu(level='Global', start_date = '2020-03-01', end_date = '2020-06-08')
 
 #Perform Rt estimation across all states, indexing by stateName
 state_rt_lst <- list()
 uniq_states <- unique(jhu_states$stateName)
 for (ii in 1:length(uniq_states)) {
+  message(sprintf("Working on state %d of %d...", ii, length(uniq_states)))
   cur_state <- uniq_states[ii]
   jhu_state_sub <- jhu_states[jhu_states$stateName == cur_state, ]
   state_rt_lst[[ii]] <-
@@ -27,6 +28,7 @@ jhu_state_out <- merge(jhu_states, state_rt, by=c('stateName','date'))
 county_rt_lst <- list()
 uniq_uid <- unique(jhu_counties$UID)
 for (ii in 1:length(uniq_uid)) {
+  message(sprintf("Working on county %d of %d...", ii, length(uniq_uid)))
   cur_uid <- uniq_uid[ii]
   jhu_county_sub <- jhu_counties[jhu_counties$UID == cur_uid,]
   county_rt_lst[[ii]] <-
@@ -44,6 +46,7 @@ jhu_county_out <- merge(jhu_counties, county_rt, by=c('UID','date'))
 global_rt_lst <- list()
 uniq_uid <- unique(jhu_global$UID)
 for (ii in 1:length(uniq_uid)) {
+  message(sprintf("Working on country %d of %d...", ii, length(uniq_uid)))
   cur_uid <- uniq_uid[ii]
   jhu_global_sub <- jhu_global[jhu_global$UID == cur_uid,]
   global_rt_lst[[ii]] <-
@@ -57,11 +60,11 @@ names(global_rt)[c(2,ncol(global_rt))] <- c('date','UID')
 global_rt$date <- as.character(global_rt$date)
 jhu_global_out <- merge(jhu_global, global_rt, by=c('UID','date'))
 
-write.table(jhu_county_out, "./COVID19-Rt/initial_estimates/jhu_county_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
-write.table(jhu_state_out, "./COVID19-Rt/initial_estimates/jhu_state_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
-write.table(jhu_global_out, "./COVID19-Rt/initial_estimates/jhu_global_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
+write.table(jhu_county_out, "jhu_county_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
+write.table(jhu_state_out, "jhu_state_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
+write.table(jhu_global_out, "jhu_global_rt.csv", quote = T, row.names = F, col.names = T, sep=',')
 
 
-write.table(jhu_county_out, "./COVID19-Rt/initial_estimates/jhu_county_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
-write.table(jhu_state_out, "./COVID19-Rt/initial_estimates/jhu_state_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
-write.table(jhu_global_out, "./COVID19-Rt/initial_estimates/jhu_global_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
+write.table(jhu_county_out, "jhu_county_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
+write.table(jhu_state_out, "jhu_state_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
+write.table(jhu_global_out, "jhu_global_rt.tsv", quote = F, row.names = F, col.names = T, sep = "\t")
