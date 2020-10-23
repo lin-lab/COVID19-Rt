ee6b0ef957c2bf723a9a959faea5dc0dd854c61c
Andy Shi <andyshi@g.harvard.edu>
2020-10-23 16:37:30 -0400

    switch to AWS hosting

--- initial_estimates/calc_initial_estimates.sh
+++ initial_estimates/calc_initial_estimates.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+base_url=https://hsph-covid-study.s3.us-east-2.amazonaws.com/JHU_Cleaned
+out_dir=raw_data
+
+if [ ! -d ${out_dir} ]
+then
+  mkdir -v ${out_dir}
+fi
+
+# download zipped files and unzip
+for ext in County State Global
+do
+    filename=JHU_COVID-19_${ext}.csv
+    out_zip=${out_dir}/${filename}.zip
+    url=${base_url}/${filename}.zip
+    wget --output-document=$out_zip $url && unzip -o $out_zip -d ${out_dir} && rm -f $out_zip
+done
+
+# call script to calculate estimates
 Rscript --vanilla calc_initial_estimates.R
 
-# zip files so we can upload to github
-zip jhu_county_rt.tsv.zip jhu_county_rt.tsv
-zip jhu_global_rt.tsv.zip jhu_global_rt.tsv
-zip jhu_state_rt.tsv.zip jhu_state_rt.tsv
-
-Rscript --vanilla upload_to_gdrive.R
+# zip results and upload to amazon s3
+for ext in county state global
+do
+  tsv=jhu_${ext}_rt.tsv
+  out_zip=${tsv}.zip
+  zip - ${tsv} > ${out_zip}
+  aws s3 cp ${out_zip} s3://hsph-covid-study/Rt-values/
+done
--- preprocess_data/preprocess_jhu.R
+++ preprocess_data/preprocess_jhu.R
@@ -3,8 +3,10 @@ library (readr)
 
 #Inputs: level (County, State, or USA), pull date from jhu ("%Y-%m-%d"), start date for the data period ("%Y-%m-%d"), end date for the data period ("%Y-%m-%d")
 load_jhu <- function(level, start_date, end_date){
-  jhu_url <- paste0("https://raw.githubusercontent.com/lin-lab/COVID-data-cleaning/master/jhu_data/cleaned_data/JHU_COVID-19_", level, ".csv")
-  input_data <- read.csv(url(jhu_url), stringsAsFactors = FALSE)
+  #jhu_url <- paste0("https://raw.githubusercontent.com/lin-lab/COVID-data-cleaning/master/jhu_data/cleaned_data/JHU_COVID-19_", level, ".csv")
+  #jhu_path <- url(jhu_url)
+  jhu_path <- paste0("raw_data/JHU_COVID-19_", level, ".csv")
+  input_data <- read.csv(jhu_path, stringsAsFactors = FALSE)
   jhu_out <- input_data[ as.Date(input_data$date) >= as.Date(start_date) & as.Date(input_data$date) <= as.Date(end_date), ]
   return(jhu_out)
 }
